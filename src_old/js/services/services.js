8da5cc5a261da7f07e96f531cd7da009c4ba33f6
prescottprue <prescottprue@hotmail.com>
2014-07-20 07:28:54 -0500

    Trigger App created with new name. Modules now included. Topbar hides on app start.

--- src_old/js/services/services.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  /* Services */
-  angular.module('roupApp.services', ['roupApp.service.firebase', 'roupApp.service.forge', 'roupApp.service.storage', 'roupApp.service.login'])
-
-})();
